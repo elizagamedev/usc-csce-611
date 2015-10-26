b90d550186a3bff7a3df619c934a709bf1855990
PENCHALA MRS <penchala@l-3d22-05.engr.sc.edu>
2015-10-26 17:32:18 -0400

    Work on project 3

--- Project3/fetch.v
+++ Project3/fetch.v
@@ -1,7 +1,6 @@
-module fetch(input clk, rst,
+module fetch(input clk, rst, stall_EX,
              input [9:0] branch_addr_EX, jtype_addr_EX, reg_addr_EX,
              input [1:0] pc_src_EX,
-             input stall_EX,
              output reg [31:0] instruction_EX,
              output reg [9:0] PC_FETCH);
     // instruction memory
@@ -17,20 +16,20 @@ module fetch(input clk, rst,
     end
     
     always @(posedge clk) begin
-        if (stall) begin
+        if (stall_EX || rst) begin
             instruction_EX = 32'b0;
-        else
+        end else begin
             // send the new instruction
             instruction_EX = mem[PC];
-            
-            // set the new PC
-            case (pc_src_EX)
-            0: PC = PC + 1
-            1: PC = PC + branch_addr_EX;
-            2: PC = PC + jtype_addr_EX; //TODO: ask dr bakos
-            3: PC = PC + reg_addr_EX; //TODO: as above
-            endcase
         end
+
+        // set the new PC
+        case (pc_src_EX)
+        0: PC = PC + 1;
+        1: PC = branch_addr_EX;
+        2: PC = jtype_addr_EX; //TODO: ask dr bakos
+        3: PC = reg_addr_EX; //TODO: as above
+        endcase
         PC_FETCH = PC;
     end
 endmodule
--- Project3/testbench.v
+++ Project3/testbench.v
@@ -6,35 +6,34 @@ module testbench();
     reg rst;
     
     // inputs
-    reg [11:0] branch_addr_EX = 0, jtype_addr_EX = 0, reg_addr_EX = 0;
     reg [3:0] pc_src_EX = 0;
-    reg stall_EX = 0;
+    reg [3:0] stall_EX = 0;
     
     // outputs
-    wire [31:0] instruction_EX;
-    wire [9:0] PC_FETCH;
+    wire [31:0] instruction_EX = 0;
+    wire [9:0] PC_FETCH = 0;
 
     // expected
-    reg [31:0] instruction_EX_expect;
-    reg [11:0] PC_FETCH_expect;
+    reg [31:0] instruction_EX_expect = 0;
+    reg [11:0] PC_FETCH_expect = 0;
 
-    fetch fe(clk, rst, branch_addr_EX[9:0], jtype_addr_EX[9:0], reg_addr_EX[9:0], pc_src_EX[1:0], stall_EX, instruction_EX, PC_FETCH);
+    fetch fe(clk, rst, stall_EX[0], instruction_EX[9:0] + PC_FETCH, instruction_EX[9:0], 10'd1, pc_src_EX[1:0], instruction_EX, PC_FETCH);
 
     always begin
-        clk=1;#5;clk=0;#5;
+        clk = 0; #5; clk = 1; #5;
     end
 
     initial begin
         $readmemh("fetch.tv", testvectors);
         vectornum = 0; errors = 0;
-        rst = 1; #5 rst = 0;
+        rst = 1; #6 rst = 0;
     end
 
     always @(negedge clk) begin
-        {instruction_EX_expect, PC_FETCH_expect, branch_addr_EX, jtype_addr_EX, reg_addr_EX, pc_src_EX, stall_EX} = testvectors[vectornum];
+        {PC_FETCH_expect, instruction_EX_expect, pc_src_EX, stall_EX} = testvectors[vectornum];
         
-        if (instruction_EX_expect[9:0] = instruction_EX || PC_FETCH_expect[9:0] != PC_FETCH) begin
-            $display("error: vector %d (expected instruction %03x, PC %03x; got instruction %03x, PC %03x)", vectornum, instruction_EX_expect, PC_FETCH_expect, instruction_EX, PC_FETCH);
+        if (instruction_EX_expect[9:0] == instruction_EX || PC_FETCH_expect[9:0] != PC_FETCH) begin
+            $display("error: vector %d (expected instruction %08x, PC_FETCH %03x; got instruction %08x, PC_FETCH %03x)", vectornum, instruction_EX_expect, PC_FETCH_expect, instruction_EX, PC_FETCH);
         end
 
         // check for end
