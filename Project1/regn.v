241ce709e2117014f79c48d17826f0d9768248cd
PENCHALA MRS <penchala@l-3d22-05.engr.sc.edu>
2015-11-19 14:18:23 -0500

    Moved stuff into root folder

--- Project1/regn.v
+++
@@ -1,7 +0,0 @@
-module regn #(parameter width=32) (input clk, input [width-1:0] d,
-                                   output reg [width-1:0] q);
-    always @(posedge clk)
-    begin
-        q <= d;
-    end
-endmodule
